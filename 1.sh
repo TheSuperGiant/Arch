aa10dbbfad9ef18a7e0cdb43280919da8eb6a30c
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2024-11-10 22:30:12 +0000

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -9,12 +9,15 @@
 #
 #echo "$USER ALL=(ALL) NOPASSWD: /usr/bin/reboot, /usr/bin/shutdown, /usr/bin/poweroff" | sudo tee -a /etc/sudoers
 #
+sudo umount -l "$drive"
 for drive in /dev/sr*; do
     if [ -b "$drive" ]; then
-        sudo fuser -km "$drive"
-        sudo umount "$drive"
+        #sudo fuser -km "$drive"
+        mount | grep "$drive" | awk '{print $3}' | while read mount_point; do
+            sudo umount "$mount_point"
+        done
         echo "Ejecting $drive..."
-        echo "test 3"
+        echo "test 4"
         eject -f "$drive"
     fi
 done
