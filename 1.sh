231e883566cec7e93b748078d36dd844c917bc27
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2024-11-10 23:27:11 +0000

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -3,24 +3,10 @@
 #-------samba----------
 #
 #
-#bash <(curl -L christitus.com/archtitus)
+bash <(curl -L christitus.com/archtitus)
 #
 #
 #
-#echo "$USER ALL=(ALL) NOPASSWD: /usr/bin/reboot, /usr/bin/shutdown, /usr/bin/poweroff" | sudo tee -a /etc/sudoers
-#
-#for drive in /dev/sr*; do
-    #if [ -b "$drive" ]; then
-        #sudo fuser -km "$drive"
-        #sudo umount -l "$drive"
-        #mount | grep "$drive" | awk '{print $3}' | while read mount_point; do
-            #sudo umount "$mount_point"
-       # done
-       # sudo umount -f "$drive"
-        #echo "Ejecting $drive..."
-        #echo "test 6"
-       # eject -f "$drive"
-    #fi
-#done
+echo "$USER ALL=(ALL) NOPASSWD: /usr/bin/reboot, /usr/bin/shutdown, /usr/bin/poweroff" | sudo tee -a /etc/sudoers
 #
 sudo reboot
