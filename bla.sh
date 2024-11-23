10b28d6970536081d95161c3677700b756234790
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2024-11-23 22:29:01 +0100

    Add files via upload

---
+++ bla.sh
@@ -0,0 +1,25 @@
+source ~/Scripts/config/config.sh
+
+declare -a App_Install__=(
+	"wine:					wine"
+	"wine_mono:				wine-mono"
+	"7_zip:					7-zip-full"
+	"minecraft_launcher:	minecraft-launcher;1"
+	"adwaita_theme:			adwaita-icon-theme"
+	
+)
+
+for app in "${App_Install__[@]}"; do
+	key="${app%%:*}"
+	if [ "$(eval echo \$App_Install__$key)" == "1" ]; then
+		value=$(echo "${app##*:}" | tr -d '[:space:]')
+		if [[ "$value" == *";"* ]]; then
+			echo "have ;"
+		else
+			echo "not ;"
+		fi
+	fi
+    echo "$key"
+    echo "$value"
+	value=""
+done
