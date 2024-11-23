551d1292ac565df45eb6e2ac121de222df45fe48
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2024-11-23 22:54:43 +0100

    Add files via upload

--- bla.sh
+++ bla.sh
@@ -4,25 +4,24 @@ declare -a App_Install__=(
 	"wine:					wine"
 	"wine_mono:				wine-mono"
 	"7_zip:					7-zip-full"
-	"minecraft_launcher:	minecraft-launcher;1"
 	"adwaita_theme:			adwaita-icon-theme"
+	"minecraft_launcher:	minecraft-launcher;1"
 	
 )
 
 for app in "${App_Install__[@]}"; do
 	key="${app%%:*}"
+	#value maby a row down in the if statemant later to do with echo writing
+	value=$(echo "${app##*:}" | tr -d '[:space:]')
 	if [ "$(eval echo \$App_Install__$key)" == "1" ]; then
-		value=$(echo "${app##*:}" | tr -d '[:space:]')
 		if [[ "$app" == *";"* ]]; then
-			#echo 1 | paru -S --noconfirm minecraft-launcher
-			#split ;
-			#echo $..| paru -S --noconfirm $value
-			echo "have ;"
+			value=$(echo "${value%%;*}" | tr -d '[:space:]')
+			number=$(echo "${app##*;}")
+			echo $number | paru -S --noconfirm $value
 		else
-			echo "not ;"
+			paru -S --noconfirm $value
 		fi
 	fi
     echo "$key"
     echo "$value"
-	value=""
 done
