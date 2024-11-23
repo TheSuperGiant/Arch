2a8fca0a6fe4ab81f2282fa419572ccadd414a5f
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2024-11-23 17:15:41 +0100

    Add files via upload

--- 1.sh
+++ 1.sh
@@ -1,10 +1,2 @@
-﻿#-------samba----------
-#read -sp "Enter your Windows  Adminstartor password: " password
-#-------samba----------
-#
-#
-bash <(curl -L christitus.com/archtitus)
-#
-#
-#
+﻿bash <(curl -L christitus.com/archtitus)
 reboot
