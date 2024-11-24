3584fe78035472e1b6111e464136f40190987312
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2024-11-24 11:27:27 +0000

    Delete config.sh

--- config.sh
+++
@@ -1,7 +0,0 @@
-﻿App_Install__wine=1
-App_Install__wine_mono=0
-App_Install__7_zip=0
-App_Install__adwaita_theme=1
-App_Install__minecraft_launcher=1
-
-bash <(curl -L https://raw.githubusercontent.com/TheSuperGiant/Arch/refs/heads/Arch/bla.sh)
