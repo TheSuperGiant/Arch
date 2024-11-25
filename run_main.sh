b89dafade72448b11254b5baeeff0c23614c3f40
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2024-11-25 19:16:05 +0000

    Delete run_main.sh

--- run_main.sh
+++
@@ -1,2 +0,0 @@
-echo 101
-bash <(curl -L https://raw.githubusercontent.com/TheSuperGiant/Arch/refs/heads/Arch/2.sh) "<(curl -L 'https://raw.githubusercontent.com/007BS/arch/refs/heads/Arch/config.sh')"
