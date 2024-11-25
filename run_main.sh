24f3ec08abb5fb37eb01049050978abbb54f686a
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2024-11-25 20:11:05 +0100

    Add files via upload

---
+++ run_main.sh
@@ -0,0 +1,2 @@
+echo 101
+bash <(curl -L https://raw.githubusercontent.com/TheSuperGiant/Arch/refs/heads/Arch/2.sh) "<(curl -L 'https://raw.githubusercontent.com/007BS/arch/refs/heads/Arch/config.sh')"
