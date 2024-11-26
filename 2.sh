d5b77ef6d54815aa3fb0cf9a23c79a6c1c08f01f
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2024-11-26 14:18:35 +0100

    Add files via upload

--- 2.sh
+++ 2.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+# Disclaimer:
+# This script is provided as-is, without any warranty or guarantee.
+# By using this script, you acknowledge that you do so at your own risk.
+# I am not responsible for any damage, data loss, or other issues that may result from the use of this script.
+
+
 sudo -v
 while true; do
     sudo -n true
