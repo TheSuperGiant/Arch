823cb336cc19d680d23d177a8300bbe5725946bb
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2024-11-27 16:51:31 +0100

    Add files via upload

--- 2.sh
+++ 2.sh
@@ -128,27 +128,22 @@ mdr /mnt/Games
 if [ -n "$StartScript" ]; then
 	md $HOME/Scripts
 	md ~/.config/autostart
-	
-	startup_location="$HOME/Scripts/startup_script.sh"
-	
-	if [ ! -f "$startup_location" ] || ! echo "$StartScript" | diff -q - "$startup_location" > /dev/null; then
-		echo -e "$StartScript" > $startup_location
-	fi
-	#autostart_location="~/.config/autostart/startup_script.desktop"
-	if [ ! -f ~/.config/autostart/startup_script.desktop ]; then
-		echo "[Desktop Entry]
-		Type=Application
-		Exec=sudo $startup_script_file_location
-		Hidden=false
-		NoDisplay=false
-		X-GNOME-Autostart-enabled=true
-		Name=My Startup Script
-		Comment=Runs my startup script at login" > ~/.config/autostart/startup_script.desktop
-	fi
+	startup_script_file_location="$HOME/Scripts/startup_script.sh"
+
+	echo -e "StartScript" > $startup_script_file_location
+
+	echo "[Desktop Entry]
+	Type=Application
+	Exec=sudo $startup_script_file_location
+	Hidden=false
+	NoDisplay=false
+	X-GNOME-Autostart-enabled=true
+	Name=My Startup Script
+	Comment=Runs my startup script at login" > ~/.config/autostart/startup_script.desktop
 
 	add_sudo "$USER ALL=(ALL) NOPASSWD: $HOME/Scripts/*"
 
-	chmod +x $startup_location
+	chmod +x $startup_script_file_location
 fi
 
 if [ -n "$add_device_labels" ]; then
@@ -354,6 +349,6 @@ add_lightdm "greeter-setup-script=/usr/bin/numlockx on" "/^\[Seat:\*\]/a"
 echo "NumLock on configuration added to [Seat:*] section."
 #numlock on at startup
 
-echo "test 28"
+echo "test 25"
 
 #sudo reboot
