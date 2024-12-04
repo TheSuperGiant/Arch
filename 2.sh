9f30f1ba3275f3fe7faecee3f20dda240ca44257
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2024-12-04 15:58:10 +0100

    Add files via upload

--- 2.sh
+++ 2.sh
@@ -27,71 +27,7 @@ if [ "$linutil__christitus" == "1" ]; then
 	curl -fsSL https://christitus.com/linux | sh
 fi
 
-
-LIGHTDM_CONF="/etc/lightdm/lightdm.conf"
-
-add_alias() {
-	alias_name=$1
-	alias_command=$2
-	if ! grep -q "^alias $alias_name" ~/.bashrc; then
-		echo "alias $alias_name=\"$alias_command\"" >> ~/.bashrc
-		eval "$alias_name() {
-			$alias_command
-		}"
-		echo "Alias '$alias_name' added and saved to ~/.bashrc."
-	fi
-}
-add_device_label() {
-	if ! sudo grep -q "LABEL=$1" /etc/fstab; then
-		fs_type=$(lsblk -o NAME,LABEL,FSTYPE | grep -w $1 | awk '{print $3}')
-		if [ -n "$fs_type" ]; then
-			mountpoint="/mnt/$1"
-			sudo bash -c "echo \"LABEL=$1 $mountpoint $fs_type defaults,nofail 0 2\" >> /etc/fstab"
-			sudo mkdir -p $mountpoint
-			sudo chown $USER:$USER $mountpoint
-			#mdr $mountpoint
-		fi
-	fi
-}
-add_function() {
-    func_name=$1
-    func_body=$2
-	if ! grep -q "^function $func_name" ~/.bashrc; then
-		echo -e "function $func_name {\n\t$func_body\n}" >> ~/.bashrc
-		eval "$func_name() {
-			$func_body
-		}"
-		echo "Function '$func_name' added and is now available."
-	fi
-}
-add_lightdm() {
-	if [[ -n "$2" && -z "$3" ]]; then
-		local third="$2"
-	else
-		local third="$3"
-	fi
-	if [ "$1" == "e" ]; then
-		if ! grep -q "^$third" "$LIGHTDM_CONF"; then
-			echo -e "\n$2" | sudo tee -a "$LIGHTDM_CONF"
-		fi
-	else
-		if ! grep -q "^$1" "$LIGHTDM_CONF"; then
-			sudo sed -i "$2 $1" "$LIGHTDM_CONF"
-		fi
-	fi
-}
-add_sudo() {
-	if ! sudo grep -q "$1" /etc/sudoers; then
-		echo "$1" | sudo tee -a /etc/sudoers
-	fi
-}
-bool() {
-	if [ "$1" == "1" ]; then
-		echo "true"
-	else
-		echo "false"
-	fi
-}
+source <(curl -L https://raw.githubusercontent.com/TheSuperGiant/Arch/refs/heads/Arch/functions.sh)
 
 add_alias md "mkdir -p \$1"
 add_alias mds "sudo mkdir -p \$1"
@@ -356,9 +292,10 @@ done
 	#qemu
 
 
+echo bool
 
 http_check $2
 
-echo "test 86"
+echo "test 87"
 
 #sudo reboot
