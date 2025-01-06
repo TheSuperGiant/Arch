cdc1cae265ffd1720689cc11b6c155b080b00b23
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2025-01-06 22:51:04 +0000

    Add files via upload

--- 2.sh
+++ 2.sh
@@ -354,6 +354,20 @@ if command -v cinnamon-session >/dev/null 2>&1; then
 	dcow $path "$updated_applets"
 fi
 
+if [ "$ipV6_disable" == 1 ]; then
+	echo "IPv6 disabled"
+	#CONFIG_FILE="/etc/sysctl.d/99-disable-ipv6.conf"
+	#sudo bash -c "cat > $CONFIG_FILE" << EOF
+	#sudo bash -c "cat > /etc/sysctl.d/99-disable-ipv6.conf" << EOF
+	#net.ipv6.conf.all.disable_ipv6 = 1
+	#net.ipv6.conf.default.disable_ipv6 = 1
+	#net.ipv6.conf.lo.disable_ipv6 = 1
+	#EOF
+	echo -e "net.ipv6.conf.all.disable_ipv6 = 1
+	net.ipv6.conf.default.disable_ipv6 = 1
+	net.ipv6.conf.lo.disable_ipv6 = 1" | sed -E 's/^[[:space:]]+//' | sudo tee -a /etc/sysctl.d/99-disable-ipv6.conf
+	sudo sysctl --system
+fi
 
 
 #------------------
