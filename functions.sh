9a6506cf224b1f3ea070351a43baded92879c33f
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2025-01-14 16:06:09 +0000

    Add files via upload

--- functions.sh
+++ functions.sh
@@ -102,12 +102,13 @@ alias dco="dconf dump /"
 par() {
 	while true; do
 		while read line; do
+			((test+=1))
 			#echo "$line"
 			if echo "$line" | grep -q "invalid or corrupted.*(PGP signature)"; then
 				local PGP_signature_error=1
 			fi
-		done < <(stdbuf -oL paru -S $@ 2>&1)
-		#done < <(paru -S $@ 2>&1)
+		done < <(paru -S $@)
+		echo $test
 		if [[ $PGP_signature_error == 1 ]]; then
 			sudo pacman -Sy archlinux-keyring --noconfirm
 			sudo pacman-key --populate archlinux
