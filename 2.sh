b5c0ff12a6582a15de839e81f042fd9d014fc117
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2025-02-01 23:04:19 +0000

    Add files via upload

--- 2.sh
+++ 2.sh
@@ -4,72 +4,6 @@
 # By using this script, you acknowledge that you do so at your own risk.
 # I am not responsible for any damage, data loss, or other issues that may result from the use of this script.
 
-App_Install__7_zip=1
-App_Install__brave=1
-App_Install__firefox=1
-App_Install__google_chrome=1
-App_Install__librewolf=1
-App_Install__opera=1
-App_Install__thorium=1
-App_Install__torbrowser=1
-App_Install__waterfox=1
-App_Install__heroic_launcher=1
-App_Install__minecraft_launcher=1
-App_Install__paradox_launcher=1
-App_Install__gnome_terminal=1
-App_Install__adwaita_theme=1
-App_Install__font_dejavu=1
-App_Install__bluetooth=1
-App_Install__wine=1
-App_Install__wine_mono=1
-App_Install__anydesk=1
-App_Install__audacity=1
-App_Install__biglybt=1
-App_Install__bleachbit=1
-App_Install__calibre=1
-App_Install__discord=1
-App_Install__dropbox=1
-App_Install__eye_of_gnome=1
-App_Install__filezilla=1
-App_Install__git=1
-App_Install__gimp=1
-App_Install__gnome_calculator=1
-App_Install__gthumb=1
-App_Install__handbrake=1
-App_Install__jitsi_meet=1
-App_Install__kcalc=1
-App_Install__keepass=1
-App_Install__keepassxc=1
-App_Install__leafpad=1
-App_Install__libreoffice=1
-App_Install__mega=1
-App_Install__mousepad=1
-App_Install__mpv_Media_player=1
-App_Install__nautilus=1
-App_Install__nomacs=1
-App_Install__notepadqq=1
-App_Install__notepadPlusPlus=1
-App_Install__numlockx=1
-App_Install__obs_studio=1
-App_Install__openoffice=1
-App_Install__pcloud=1
-App_Install__peazip=1
-App_Install__pidgin=1
-App_Install__scrcpy=1
-App_Install__session=1
-App_Install__signal=1
-App_Install__smplayer=1
-App_Install__steam=1
-App_Install__teamviewer=1
-App_Install__thunderbird=1
-App_Install__tigervnc=1
-App_Install__vim=1
-App_Install__virtualbox=1
-App_Install__visual_studio_code=1
-App_Install__vlc=1
-App_Install__vuze=1
-App_Install__wire=1
-App_Install__xed=1
 
 http_check() {
 	if [[ "$1" == *"http"* ]]; then
@@ -442,6 +376,221 @@ fi
 echo "------------------------------------"
 echo "|    Updating default program..    |"
 echo "------------------------------------"
+
+default_app (){
+	if [[ "$1" == $(ls /usr/share/applications/ | grep -i $1) ]] && ! [[ "$1" == $(xdg-mime query default "$2") ]]; then
+		xdg-mime default "$1" "$2"
+		echo "${1%%.desktop} ${2##*/} - Updated to the default program"
+	else
+		echo "${1%%.desktop} ${2##*/} - Already set as the default program"
+	fi
+	echo "------------------------------------"
+}
+
+declare -a Default_Apps=(
+	"biglybt:				biglybt.desktop"
+	"brave:					brave-browser.desktop"
+	"calibre:				calibre-ebook-viewer.desktop"
+	"eye_of_gnome:				org.gnome.eog.desktop"
+	"firefox:				firefox.desktop"
+	"google_chrome:			google-chrome.desktop"
+	"keepass:				keepass.desktop"
+	"keepassxc:				org.keepassxc.KeePassXC.desktop"
+	"leafpad:				leafpad.desktop"
+	"libreoffice_database:			libreoffice-base.desktop"
+	"libreoffice_graphics:			libreoffice-draw.desktop"
+	"libreoffice_math:			libreoffice-math.desktop"
+	"libreoffice_presentation:			libreoffice-impress.desktop"
+	"libreoffice_spreadsheet:			libreoffice-calc.desktop"
+	"libreoffice_word:			libreoffice-writer.desktop"
+	"librewolf:				librewolf.desktop"
+	"mousepad:					org.xfce.mousepad.desktop"
+	"mpv_Media_player:					mpv.desktop"
+	"nautilus:					org.gnome.Nautilus.desktop"
+	"nemo:					nemo.desktop"
+	"nomacs:				org.nomacs.ImageLounge.desktop"
+	"notepadqq:				notepadqq.desktop"
+	"notepad++:		notepad++.desktop"
+	"openoffice_database:			openoffice4-base.desktop"
+	"openoffice_graphics:			openoffice4-draw.desktop"
+	"openoffice_math:			openoffice4-math.desktop"
+	"openoffice_presentation:			openoffice4-impress.desktop"
+	"openoffice_spreadsheet:			openoffice4-calc.desktop"
+	"openoffice_word:			openoffice4-writer.desktop"
+	"opera:					opera.desktop"
+	"peazip:				peazip.desktop"
+	"smplayer:				smplayer.desktop"
+	"thorium:				thorium-browser.desktop"
+	"torbrowser:			torbrowser-launcher"
+	"thunderbird:			org.mozilla.Thunderbird.desktop"
+	##"virtualbox:			virtualbox.desktop"
+	#"visual_studio_code:	visual-studio-code-bin"
+	"vlc:					vlc.desktop"
+	"vuze:					vuze.desktop"
+	"waterfox:				waterfox.desktop"
+	"wire:					wire-desktop.desktop"
+)
+
+declare -a Default_category=(
+	"Default_archive:		application"
+	"Default_browser:	x-scheme-handler/http x-scheme-handler/https"
+	"Default_e_books:		application"
+	"Default_file_manager:		inode/directory"
+	"Default_mail:		x-scheme-handler/mailto"
+	"Default_multimedia_audio:		audio"
+	"Default_multimedia_video:		video"
+	"Default_multimedia_image:		image"
+	"Default_office_database:		application"
+	"Default_office_graphics:		application"
+	"Default_office_math:		application"
+	"Default_office_presentation:		application"
+	"Default_office_spreadsheet"
+	"Default_office_word:		application"
+	"Default_pdf:		application/pdf"
+	"Default_scripts:		text/javascript text/xml text/x-python text/x-c text/x-c++ text/x-java text/x-shellscript text/x-php text/x-perl text/x-ruby text/x-lua text/x-markdown text/x-yaml text/x-toml text/x-sql text/x-asm text/x-csharp text/x-go text/x-rust text/x-d text/x-swift text/x-kotlin text/x-scala text/x-haskell text/x-erlang text/x-elisp text/x-lisp text/x-clojure text/x-scheme text/x-ocaml text/x-vbscript text/x-pascal text/x-fortran text/x-r"
+	"Default_text_files:		text/plain"
+	"Default_torrent:		application/x-bittorrent"
+	"Default_web_edditor:		text/html text/css"
+)
+
+declare -a Default_archive=(
+	"peazip:	x-7z-compressed x-zip-compressed x-rar-compressed x-tar x-gzip x-bzip2 x-xz x-lzma x-ace-compressed x-arj x-lha x-tar+gzip x-tar+bzip2 x-tar+xz x-iso9660-image x-cpio x-lz4 x-zip x-rar x-bzip x-bz2"
+)
+
+declare -a Default_browser=(
+	"brave"
+	"firefox"
+	"google_chrome"
+	"librewolf"
+	"opera"
+	"thorium"
+	"torbrowser"
+	"waterfox"
+)
+
+declare -a Default_e_books=(
+	"calibre:	epub+zip x-mobipocket-ebook x-msbook x-fictionbook+xml x-sony-bbeb x-azw3 x-kobo-book x-chm x-html x-plain-text x-lrf"
+)
+
+declare -a Default_file_manager=(
+	"nautilus"
+	"nemo"
+)
+
+declare -a Default_mail=(
+	"brave"
+	"firefox"
+	"google_chrome"
+	"librewolf"
+	"opera"
+	"thorium"
+	"thunderbird"
+	"torbrowser"
+	"waterfox"
+)
+
+declare -a Default_multimedia_audio=(
+	"smplayer:	wav mpeg aac x-ms-wma flac alac mp4 ogg opus x-ape midi x-midi x-matroska"
+	"mpv_Media_player:	wav mpeg aac x-ms-wma flac alac mp4 ogg opus x-ape midi x-midi x-matroska x-vorbis basic vnd.wave"
+	"vlc:	wav mpeg aac x-ms-wma flac alac mp4 ogg opus x-ape midi x-midi x-matroska x-vorbis basic vnd.wave 3gpp 3gpp2 x-mod x-s3m x-it x-xm"
+)
+
+declare -a Default_multimedia_video=(
+	"smplayer:	avi flv mp4 mpeg quicktime vnd.rn-realvideo webm x-matroska x-ms-asf x-ms-wmv x-msvideo x-ogm+ogg x-theora 3gp 3gpp divx mp4v-es msvideo ogg vivo vnd.divx x-anim x-avi x-flc x-fli x-flic x-flv x-m4v x-mng x-mpeg x-mpeg2 x-ms-afs x-ms-asx x-ms-wm x-ms-wvx x-ms-wvxvideo x-nsv x-theora+ogg"
+	"mpv_Media_player:	avi flv mp4 mpeg quicktime vnd.rn-realvideo webm x-matroska x-ms-asf x-ms-wmv x-msvideo x-ogm+ogg x-theora 3gpp divx mp4v-es msvideo ogg vivo vnd.divx x-anim x-avi x-flc x-fli x-flic x-flv x-m4v x-mng x-mpeg x-mpeg2 x-ms-afs x-ms-asx x-ms-wm x-ms-wvx x-ms-wvxvideo x-nsv x-theora+ogg 3gpp2 dv fli mkv mp2t vnd.avi vnd.mpegurl x-mpeg3 x-ms-wmx x-ogm"
+	"vlc:	avi flv mp4 mpeg quicktime vnd.rn-realvideo webm x-matroska x-ms-asf x-ms-wmv x-msvideo x-ogm+ogg x-theora 3gp 3gpp divx mp4v-es msvideo ogg vivo vnd.divx x-anim x-avi x-flc x-fli x-flic x-flv x-m4v x-mng x-mpeg x-mpeg2 x-ms-afs x-ms-asx x-ms-wm x-ms-wvx x-ms-wvxvideo x-nsv x-theora+ogg 3gpp2 dv fli mp2t vnd.mpegurl x-ms-wmx x-ogm mpeg-system x-mpeg-system x-ms-asf-plugin"
+)
+
+declare -a Default_multimedia_image=(
+	"eye_of_gnome:	bmp gif jpeg jxl png tiff webp x-ico x-portable-bitmap x-portable-graymap x-portable-pixmap x-xbitmap x-xpixmap jpg pjpeg svg+xml svg+xml-compressed vnd.wap.wbmp x-bmp x-gray x-icb x-icns x-pcx x-png x-portable-anymap "
+	"nomacs:	avif bmp gif heic heif jpeg jxl png tiff webp x-eps x-ico x-portable-bitmap x-portable-graymap x-portable-pixmap x-xbitmap x-xpixmap"
+)
+
+declare -a Default_office_database=(
+	"libreoffice_database:	vnd.oasis.opendocument.database x-sql vnd.ms-access vnd.ms-access.accdb x-dbase x-dbf vnd.ms-office x-msaccess mdb"
+	"openoffice_database:	vnd.oasis.opendocument.database x-sql vnd.ms-access vnd.ms-access.accdb x-dbase x-dbf vnd.ms-office x-msaccess mdb"
+)
+
+declare -a Default_office_graphics=(
+	"libreoffice_graphics:	vnd.oasis.opendocument.graphics vnd.oasis.opendocument.graphics-template vnd.sun.xml.draw vnd.sun.xml.draw.template vnd.stardivision.draw vnd.openxmlformats-officedocument.drawingml.diagram+xml vnd.visio vnd.visio2013 x-coreldraw x-coreldrawtemplate x-coreldrawpattern x-coreldrawpresentation x-fig x-pdf"
+	"openoffice_graphics:	vnd.oasis.opendocument.graphics vnd.oasis.opendocument.graphics-template vnd.sun.xml.draw vnd.sun.xml.draw.template vnd.stardivision.draw x-fig x-pdf"
+)
+
+declare -a Default_office_math=(
+	"libreoffice_math:	vnd.oasis.opendocument.formula vnd.oasis.opendocument.formula-template vnd.sun.xml.math x-tex x-latex x-texinfo x-troff x-dvi"
+	"openoffice_math:	vnd.oasis.opendocument.formula vnd.oasis.opendocument.formula-template vnd.sun.xml.math x-tex x-latex x-texinfo x-troff x-dvi"
+)
+
+declare -a Default_office_presentation=(
+	"libreoffice_presentation:	vnd.oasis.opendocument.presentation vnd.oasis.opendocument.presentation-template vnd.openxmlformats-officedocument.presentationml.presentation vnd.openxmlformats-officedocument.presentationml.template vnd.ms-powerpoint vnd.ms-powerpoint.presentation.macroEnabled.12 vnd.ms-powerpoint.template.macroEnabled.12 vnd.ms-powerpoint.addin.macroEnabled.12 vnd.ms-powerpoint.slideshow.macroEnabled.12 vnd.sun.xml.impress vnd.sun.xml.impress.template vnd.stardivision.impress vnd.ms-powerpoint.slideshow.macroEnabled.12 x-iwork-keynote-sffkey vnd.apple.keynote"
+	"openoffice_presentation:	vnd.oasis.opendocument.presentation vnd.oasis.opendocument.presentation-template vnd.openxmlformats-officedocument.presentationml.presentation vnd.openxmlformats-officedocument.presentationml.template vnd.ms-powerpoint vnd.ms-powerpoint.presentation.macroEnabled.12 vnd.ms-powerpoint.template.macroEnabled.12 vnd.ms-powerpoint.addin.macroEnabled.12 vnd.ms-powerpoint.slideshow.macroEnabled.12 vnd.sun.xml.impress vnd.sun.xml.impress.template vnd.stardivision.impress"
+)
+
+declare -a Default_office_spreadsheet=(
+	"libreoffice_spreadsheet:	application/vnd.oasis.opendocument.spreadsheet application/vnd.oasis.opendocument.spreadsheet-template application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet application/vnd.ms-excel.sheet.macroEnabled.12 application/vnd.ms-excel.template.macroEnabled.12 application/vnd.ms-excel.addin.macroEnabled.12 application/vnd.ms-excel.sheet.binary.macroEnabled.12 text/csv text/tab-separated-values application/vnd.lotus-1-2-3 application/vnd.stardivision.calc application/vnd.sun.xml.calc application/vnd.sun.xml.calc.template application/x-dbase application/x-dbf application/x-dos_ms_excel application/x-excel application/x-xls application/x-msexcel application/x-quattro-pro application/x-sylk text/spreadsheet application/vnd.gnumeric application/x-gnumeric application/x-spreadsheet application/vnd.openxmlformats-officedocument.spreadsheetml.template"
+	"openoffice_spreadsheet:	application/vnd.oasis.opendocument.spreadsheet application/vnd.oasis.opendocument.spreadsheet-template application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet application/vnd.ms-excel.sheet.macroEnabled.12 application/vnd.ms-excel.template.macroEnabled.12 application/vnd.ms-excel.addin.macroEnabled.12 application/vnd.ms-excel.sheet.binary.macroEnabled.12 text/csv text/tab-separated-values application/vnd.lotus-1-2-3 application/vnd.stardivision.calc application/vnd.sun.xml.calc application/vnd.sun.xml.calc.template application/x-dbase application/x-dbf application/x-dos_ms_excel application/x-excel application/x-xls application/x-msexcel application/x-quattro-pro application/x-sylk text/spreadsheet application/vnd.gnumeric application/x-gnumeric application/x-spreadsheet application/vnd.openxmlformats-officedocument.spreadsheetml.template"
+)
+
+declare -a Default_office_word=(
+	"libreoffice_word:	msword rtf vnd.ms-works vnd.oasis.opendocument.text vnd.oasis.opendocument.text-template vnd.openxmlformats-officedocument.wordprocessingml.document vnd.stardivision.writer vnd.wordperfect x-abiword"
+	"openoffice4-writer.desktop:	msword rtf vnd.ms-works vnd.oasis.opendocument.text vnd.oasis.opendocument.text-template vnd.openxmlformats-officedocument.wordprocessingml.document vnd.stardivision.writer vnd.wordperfect x-abiword"
+)
+
+declare -a Default_pdf=(
+	"brave"
+	"firefox"
+	"google_chrome"
+	"librewolf"
+	"opera"
+	"thorium"
+	"torbrowser"
+	"waterfox"
+)
+
+declare -a Default_scripts=(
+	"notepad++"
+	"notepadqq"
+)
+
+declare -a Default_text_files=(
+	"leafpad"
+	"mousepad"
+)
+
+declare -a Default_torrent=(
+	"biglybt"
+	"vuze"
+)
+
+declare -a Default_web_edditor=(
+	""
+)
+
+for default in "${Default_category[@]}"; do
+	category="${default%%:*}"
+	category_verb="${category}__app"
+	default_app=${!category_verb}; default_app="${default_app,,}"
+	if [ -n "$default_app" ]; then
+		program_install_name=$(printf "%s\n" "${Default_Apps[@]}" | grep "^$default_app:" | awk -F: '{print $2}' | sed -E 's/^[[:space:]]+//')
+		file_association_type=$(echo "${default##*:}" | sed -E 's/^[[:space:]]+//')
+		if [[ "$file_association_type" == */* ]]; then
+			for asso in $file_association_type; do
+				if [[ -n $(declare -n list="$category"; printf "%s\n" "${list[@]}" | grep -x "^$default_app") ]]; then
+					default_app "$program_install_name" "$asso"
+				fi
+			done
+		else
+			catergory_scheme=$(echo "${default##*:}" | sed -E 's/^[[:space:]]+//')
+			for asso in $(declare -n list="$category"; printf "%s\n" "${list[@]}" | grep "^$default_app:" | awk -F: '{print $2}' | sed -E 's/^[[:space:]]+//'); do
+				if [[ "$catergory_scheme" == "$default" ]]; then
+					default_app "$program_install_name" "$asso"
+				else
+					default_app "$program_install_name" "$catergory_scheme/$asso"
+				fi
+			done
+		fi
+	fi
+done
 
 
 #------------------
