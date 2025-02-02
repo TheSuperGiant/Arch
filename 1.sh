a37ca86d71863295040e37940b320bcb3bdb4d6c
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2025-02-02 21:26:32 +0000

    Add files via upload

--- 1.sh
+++ 1.sh
@@ -16,27 +16,53 @@ chmod +x archtitus.sh
 ./archtitus.sh
 
 source <(curl -s -L $1)
+source <(curl -s -L https://raw.githubusercontent.com/TheSuperGiant/Arch/refs/heads/Arch/program_install_list.sh)
 
-echo $1
-echo $login_screen
-echo $terminal
-echo $desktop_environment
+#echo $1
+#echo $login_screen
+#echo $terminal
+#echo $desktop_environment
 
-echo "The script is paused. Press Enter to continue..."
-read
-echo "Continuing the script..."
+list() {
+	local array=("${!1}")
+	for item in "${array[@]}"; do
+        if grep -q "$2" <<< "$item"; then
+			echo $(echo "${item##*:}" | sed -E 's/^[[:space:]]+//') && return 0
+		fi
+    done
+}
 
-lightdm="lightdm lightdm-gtk-greeter"
-cinnamon="cinnamon"
-terminal="gnome-terminal"
-install="$lightdm $cinnamon"
-login_manager="lightdm"
+declare -a login_sc=(
+	"kast:	kast"
+	"lightdm:	lightdm lightdm-gtk-greeter"
+	"pietje:	pietje"
+)
+
+declare -a desktop_env=(
+	"kast:	kast"
+	"cinnamon:	cinnamon"
+	"pietje:	pietje"
+)
+
+install=$(list desktop_env[@] "cinnamon")
+install+=" $( list login_sc[@] "lightdm")"
+install+=" $( list App_Install__[@] "gnome_terminal")"
+login_manager=$(list login_sc[@] "lightdm" | awk '{print $1}')
+
+
+#lightdm="lightdm lightdm-gtk-greeter"
+#cinnamon="cinnamon"
+#terminal="gnome-terminal"
+#install="$lightdm $cinnamon $terminal"
+#login_manager="lightdm"
 
 
 arch-chroot /mnt /bin/bash <<EOF
-	pacman -S $cinnamon --noconfirm
-	lightdm_service
+	pacman -S $install --noconfirm
 	systemctl enable $login_manager
 EOF
+
+echo $install
+echo $login_manager
 
 #reboot
