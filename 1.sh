c46c405edffa9670d1b7fa198da807eb1ad51690
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2025-02-05 13:01:12 +0100

    Add files via upload

--- 1.sh
+++ 1.sh
@@ -52,7 +52,7 @@ mkdir -p /mnt/etc/xdg/autostart
 cat << 'EOF' > /mnt/etc/xdg/autostart/firstboot.desktop
 [Desktop Entry]
 Type=Application
-Exec=gnome-terminal -- bash -c 'nm-online -q && sleep 1 && bash <(curl -fsSL $2); exec bash'
+Exec=gnome-terminal -- bash -c "nm-online -q && sleep 1 && bash <(curl -fsSL $2); exec bash"
 Hidden=false
 NoDisplay=false
 X-GNOME-Autostart-enabled=true
