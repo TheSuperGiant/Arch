595b99c8913f8826feea297adfc2481b3d0a6ba9
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2025-04-16 12:52:08 +0200

    Add files via upload

--- 1.sh
+++ 1.sh
@@ -85,7 +85,7 @@ Exec=/bin/bash -c "nm-online -q && sleep 1 && bash <(curl -fsSL $2); exec bash"
 Terminal=true
 EOF
 
-echo $username
+echo $USERNAME
 
 #echo "$USER ALL=(ALL) NOPASSWD: /dev/shm/2.sh" | sudo tee -a /mnt/etc/sudoers
 echo " ALL=(ALL) NOPASSWD: /dev/shm/2.sh" | sudo tee -a /mnt/etc/sudoers
