803746dbf43ad2cfc62356e367ddfe1800211eb2
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2025-11-03 20:31:27 +0000

    Delete parts/adding_device.sh

--- parts/adding_device.sh
+++
@@ -1,7 +0,0 @@
-echo "------------------------------------"
-echo "|   Adding mounted partitions...   |"
-echo "------------------------------------"
-
-if [ -n "$add_device_labels" ]; then
-	add_device_label ${add_device_labels[@]}
-fi
