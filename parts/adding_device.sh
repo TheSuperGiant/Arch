c100f7bb6d8e93883ec3884587ab4e3bb8aad617
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2025-11-03 20:25:31 +0000

    Add files via upload

--- parts/adding_device.sh
+++ parts/adding_device.sh
@@ -1,3 +1,7 @@
+echo "------------------------------------"
+echo "|   Adding mounted partitions...   |"
+echo "------------------------------------"
+
 if [ -n "$add_device_labels" ]; then
 	add_device_label ${add_device_labels[@]}
 fi
